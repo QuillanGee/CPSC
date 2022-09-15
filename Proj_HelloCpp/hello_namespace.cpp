fcacc3c3180f9c1eea05bd0377877af80d1e9e63
QuillanGee <92282756+QuillanGee@users.noreply.github.com>
2022-09-14 22:31:52 -0700

    Create hello_namespace.cpp

---
+++ Proj_HelloCpp/hello_namespace.cpp
@@ -0,0 +1,10 @@
+/// @hello_namespace.cpp <hello_namespace>
+/// @brief <This file prints Hello C++! and ends in a new line>
+/// @author <Quillan Gee>
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "Hello C++!" << endl;
+   return 0;
+}
