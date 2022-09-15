d9e9fd87ba61e0d903cc91af772f008c40870fa3
QuillanGee <92282756+QuillanGee@users.noreply.github.com>
2022-09-14 22:31:39 -0700

    Create hello.cpp

---
+++ Proj_HelloCpp/hello.cpp
@@ -0,0 +1,9 @@
+/// @hello.cpp <hello>
+/// @brief <This file outputs hello c++>
+/// @author <Quillan Gee>
+#include <iostream>
+int main()
+{
+   std::cout << "Hello C++!" << std::endl;
+   return 0;
+}
