8cf6a307026ce7190653b36e8a67701e64825f2d
QuillanGee <92282756+QuillanGee@users.noreply.github.com>
2022-10-26 19:28:06 -0700

    Create Vector3D.h

---
+++ Project Vector3D/Vector3D.h
@@ -0,0 +1,18 @@
+#if !defined(VECTOR3D_H)
+#define VECTOR3D_H
+#include <vector>
+#include <iostream>
+class Vector3D
+{
+public:
+  Vector3D();
+  Vector3D(double dX, double dY, double dZ);
+  ~Vector3D();
+  double dot(Vector3D vec3D);
+  void display();
+
+private:
+  std::vector<double> m_vector = {0,0,0};
+
+};
+#endif
